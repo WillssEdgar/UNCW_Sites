53b820c3588e313b0f3c403f39d948b33c66c936
WillssEdgar <wse8520@uncw.edu>
2024-03-17 10:37:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# csc315_team_edgar_burgess_project
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/csc315_team_edgar_burgess_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.csc315_team_edgar_burgess_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
