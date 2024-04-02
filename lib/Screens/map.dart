78a0fa07c8fd1d1e1b2aaf2ac01a663c64ec7614
WillssEdgar <wse8520@uncw.edu>
2024-04-02 09:27:20 -0400

    List view

--- lib/Screens/map.dart
+++ lib/Screens/map.dart
@@ -1,3 +1,4 @@
+import 'package:csc315_team_edgar_burgess_project/site_class.dart';
 import 'package:flutter/material.dart';
 
 class Map extends StatefulWidget {
@@ -8,15 +9,59 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  List<Site> sites = SiteData().sites;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
+      body: ListView.builder(
+        itemCount: sites.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              tileColor: Colors.white,
+              title: Text(sites[index].name),
+              leading: const Icon(
+                Icons.location_on,
+                color: Colors.teal,
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => _DetailScreen(
+                            site: sites[index],
+                          )));
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _DetailScreen extends StatelessWidget {
+  final Site site;
+
+  const _DetailScreen({Key? key, required this.site}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail View"),
+      ),
+      body: Center(
         child: Column(
-          children: [Text("Hawks Eye")],
+          children: [
+            Text(" Site Name: ${site.name}"),
+            Image.asset(site.image),
+            Text("${site.description}")
+          ],
         ),
       ),
     );
